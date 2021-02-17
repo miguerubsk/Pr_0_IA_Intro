ea92da249b82ce1213c1c7b5f4dd874d1290c3c5
Miguel González García <miguerubsk@gmail.com>
2021-02-17 12:57:42 +0100

    Update Practica0.java

--- src/practica0/Practica0.java
+++ src/practica0/Practica0.java
@@ -14,8 +14,15 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 package practica0;
+
+import Ficheros.EscribirFichero;
+import Ficheros.LeerFichero;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -23,16 +30,46 @@ package practica0;
  */
 public class Practica0 {
 
+    private final static Scanner reader = new Scanner(System.in);
+
+    public static Alumno crearAlumno() {
+        System.out.println("Nombre: ");
+        String nombre = reader.next();
+        System.out.println("Apellido: ");
+        String apellido = reader.next();
+        System.out.println("DNI: ");
+        int dni = reader.nextInt();
+        System.out.println("E-Mail: ");
+        String email = reader.next();
+        return new Alumno(nombre, apellido, dni, email);
+    }
+
+    public static ArrayList<Alumno_IA> buscarImpares(ArrayList<Alumno_IA> alumnos) {
+        ArrayList<Alumno_IA> aux = new ArrayList<>();
+        for (int i = 0; i < alumnos.size(); i++) {
+            if (alumnos.get(i).getDni() % 2 != 0) {
+                aux.add(alumnos.get(i));
+            }
+        }
+        return aux;
+    }
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Alumno_IA alumno = new Alumno_IA();
-        
-//        alumno.crearAlumno();
-        alumno.CalcularNotaPracticas();
-        System.out.println(alumno);
+        ArrayList<Alumno_IA> alumnos = new ArrayList<>();
+        ArrayList<Alumno_IA> impares;
+        try {
+            LeerFichero datos = new LeerFichero("datos.txt");
+            for (int i = 0; i < datos.getNumAlumnos(); i++) {
+                alumnos.add(new Alumno_IA(i, datos.getAlumno(i)));
+                System.out.println(datos.getAlumno(i) + "\n");
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(Practica0.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
 }
